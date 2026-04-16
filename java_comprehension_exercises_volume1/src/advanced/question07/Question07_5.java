b438acbd3cdc570c26f0301a7dbe125b9663a8f5
uedashi <uedashi@C010422996.zipangu.cac.co.jp>
2026-04-16 14:32:05 +0900

    Q07_5 途中です。

--- java_comprehension_exercises_volume1/src/advanced/question07/Question07_5.java
+++ java_comprehension_exercises_volume1/src/advanced/question07/Question07_5.java
@@ -1,6 +1,8 @@
 package advanced.question07;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Question07_5 {
 
@@ -14,5 +16,29 @@ public class Question07_5 {
 		int[] amounts = new int[5];
 
 		// TODO: 実装ここから
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int totalPrice = 0;
+
+		for (int i = 0; i < 5; i++) {
+			System.out.println(itemNames[i] + "の購入数>");
+			amounts[i] = Integer.parseInt(br.readLine());
+			totalPrice += amounts[i] * prices[i];
+		}
+		if (totalPrice >= 5000) {
+			int reducedPrice = (int) (totalPrice * 0.9);
+			int PriceWithTaxD = (int) (reducedPrice * 1.1);
+			System.out.println("割引前合計：" + totalPrice + "円");
+			System.out.println("割引適用(10%OFF)：" + reducedPrice + "円");
+			System.out.println("税込合計：" + PriceWithTaxD);
+		} else {
+			int PriceWithTax = (int) (totalPrice * 1.1);
+			System.out.println("割引前合計：" + totalPrice + "円");
+			System.out.println("税込合計：" + PriceWithTax);
+		}
+
+		for (int i = 0; i < 5; i++) {
+			System.out.println(itemNames[i] + "：単価" + prices[i] + "円(税込" + (int) (prices[i] * 1.1) + "円) ×" + amounts[i]
+					+ "個 =" + amounts[i] * prices[i] + "円");
+		}
 	}
 }
