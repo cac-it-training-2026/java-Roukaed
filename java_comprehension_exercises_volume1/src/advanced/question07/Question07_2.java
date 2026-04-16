0278012487b9f7fbdea59b05ade1b71c2ae061c7
uedashi <uedashi@C010422996.zipangu.cac.co.jp>
2026-04-16 13:48:04 +0900

    Q07_2 完了しました。

--- java_comprehension_exercises_volume1/src/advanced/question07/Question07_2.java
+++ java_comprehension_exercises_volume1/src/advanced/question07/Question07_2.java
@@ -1,6 +1,8 @@
 package advanced.question07;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Question07_2 {
 
@@ -11,7 +13,20 @@ public class Question07_2 {
 		// 単価（値は変更可）
 		int[] prices = { 100, 200, 300, 400, 500 };
 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int[] amounts = new int[5];
+
 		// TODO: 実装ここから
-		
+		for (int i = 0; i < 5; i++) {
+			System.out.println(itemNames[i] + "の購入数:>");
+			amounts[i] = Integer.parseInt(br.readLine());
+		}
+
+		int totalPrices = 0;
+		for (int i = 0; i < 5; i++) {
+			System.out.println(itemNames[i] + "：" + (amounts[i] * prices[i]) + "円");
+			totalPrices += (amounts[i] * prices[i]);
+		}
+		System.out.println("合計金額：" + totalPrices + "円");
 	}
 }
